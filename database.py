f0845ad4896cc3313b89a28e605280f42650881e
npeyre <90184564+npeyre@users.noreply.github.com>
2024-10-18 20:27:18 +0100

    Updated for PostgresSQL for Heroku

--- database.py
+++ database.py
@@ -1,12 +1,31 @@
 from flask import g
-import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 
 def connect_db():
-    sql = sqlite3.connect('/Users/nataliepeyre/Documents/CD/UDEMY_FLASK_COURSE/qna_app/questions.db')
-    sql.row_factory = sqlite3.Row
-    return sql
+    # Added URI from Heroku
+    conn = psycopg2.connect('postgres://uelmv0cs53ceh8:p14088bb491a08e41ebe57c7da4ec0ed9e1fbafe8fb7b6e1f0050063807ee4441@ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d9r6pgeht3ff6q', cursor_factory=DictCursor)
+    conn.autocommit = True # This is optional. In Postgres the connection object and curser object are separate, the commit is on the connection, and the queries are run on the cursor
+    sql = conn.cursor() # Creates a cursor to run the commands
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect_db()
+
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+    
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = db[1]
+
+    return g.postgres_db_cur
+
+def init_db():
+    db = connect_db()
+
+    # Create tables as per schema.sql
+    db[1].execute(open('schema.sql', 'r').read())
+
+    # Close connection and the curser
+    db[1].close()
+    db[0].close()
